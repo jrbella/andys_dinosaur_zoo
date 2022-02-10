c5500ca110a7ce8e4c911ad2b8f88feb3f02f574
jrbella <jbella@icstars.org>
2022-02-09 23:10:31 -0600

    adding additonal menu work

---
+++ andy_game.py
@@ -0,0 +1,24 @@
+from andy_player import Player
+from andy_dino_zoo import Zoo
+
+
+
+def game():
+    print("Starting game")
+    print("Hello player, please enter your name")
+    player_name = str(input())
+    print("Hello, " + player_name + ", Welcome to Andy's Dino Zoo Adventure")
+    game_state = True
+    while game_state:
+        game_state = False
+    print("Exiting the game")
+    game_state = False
+
+
+# create player from input
+
+
+
+#This goes to the main file
+game()
+
